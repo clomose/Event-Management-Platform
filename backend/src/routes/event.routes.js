f5147f203a74e7b37ee5895bb1bfd37e50f9468a
clomose <smartbroutkarsh@gmail.com>
2025-02-08 23:37:14 +0530

    Routes created

--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -1,10 +1,26 @@
 import { Router } from "express";
-import { createEvent } from "../controllers/event.controller.js";
+import { createEvent, 
+    getEvents, 
+    getEventById,
+    updateEvent,
+    updateEventImage,
+    deleteEvent, 
+    getEventsByUser, 
+    filterEvents,
+    registeredUsers} from "../controllers/event.controller.js";
 import { verifyUser } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
 
 router.post("/create", verifyUser, upload.single("image"), createEvent);
+router.get("/my-events", verifyUser, getEventsByUser);
+router.get("/event/:id", verifyUser, getEventById);
+router.put("/event/:id", verifyUser, updateEvent);
+router.put("/image/:id/", verifyUser, upload.single("image"), updateEventImage);
+router.delete("/event/:id", verifyUser, deleteEvent);
+router.get("/events", verifyUser, getEvents);
+router.get("/events/filter", verifyUser, filterEvents);
+router.get("/event/registered-users/:id", verifyUser, registeredUsers);
 
 export {router};
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, getCurrentUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, getCurrentUser, getRegisteredEvents, registerUserToEvent, getUserEvents} from "../controllers/user.controller.js";
 import { verifyUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -7,5 +7,9 @@ const router = Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/current-user", verifyUser, getCurrentUser);
+router.get("/registered-events", verifyUser, getRegisteredEvents);
+router.post("/register-to-event", verifyUser, registerUserToEvent);
+router.get("/user-events", verifyUser, getUserEvents);
 
 export {router};
+
